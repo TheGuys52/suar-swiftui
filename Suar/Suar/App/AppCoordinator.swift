6bf45cb01c64b68cd952234cd97866eee1cd8972
dimasdaffa <dimas.de2004@gmail.com>
2026-08-25 14:17:40 +0700

    feat: implement AppCoordinator for navigation management

--- Suar/Suar/App/AppCoordinator.swift
+++ Suar/Suar/App/AppCoordinator.swift
@@ -5,4 +5,22 @@
 //  Created by DIMAS DAFFA ERNANDA on 24/08/26.
 //
 
-import Foundation
+import SwiftUI
+
+@MainActor
+@Observable
+public final class AppCoordinator: CoordinatorProtocol {
+    public var router: Router
+    
+    public init(router: Router = Router()) {
+        self.router = router
+    }
+    
+    @ViewBuilder
+    public func start() -> some View {
+        // TODO: [@Team-Nav] Setup NavigationStack(path:) menggunakan router.path[cite: 2]
+        // TODO: [@Team-Nav] Tambahkan modifier .navigationDestination(for: AppRoute.self) dan modal sheet[cite: 2]
+        // TODO: [@Team-UI] Hubungkan ke root view awal (HomeView)[cite: 1]
+        Text("Suar Root Screen")
+    }
+}
