ed152cb9d9267402853aeba2c126c384bb0d2e1c
dimasdaffa <dimas.de2004@gmail.com>
2026-08-25 13:59:37 +0700

    feat: add initial model files for Script, ScriptBlock, ScriptBlockType, and ScriptPage

---
+++ Suar/Suar/Core/Storage/Models/ScriptBlock.swift
@@ -0,0 +1,8 @@
+//
+//  ScriptBlock.swift
+//  Suar
+//
+//  Created by DIMAS DAFFA ERNANDA on 25/08/26.
+//
+
+import Foundation
---
+++ Suar/Suar/Core/Storage/Models/ScriptBlockType.swift
@@ -0,0 +1,8 @@
+//
+//  ScriptBlockType.swift
+//  Suar
+//
+//  Created by DIMAS DAFFA ERNANDA on 25/08/26.
+//
+
+import Foundation
