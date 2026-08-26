9c1a77aae7e870633e3ff35d6dd3d96c5266e61b
= <=>
2026-08-26 15:40:10 +0700

    feat: menabahkan desain card untuk homepage

---
+++ Suar/Suar/Features/Home/Views/ReadingCard.swift
@@ -0,0 +1,49 @@
+//
+//  ReadingCard.swift
+//  Suar
+//
+//  Created by Ivan Putra Pratama on 26/08/26.
+//
+
+import SwiftUI
+
+struct ReadingCard: View {
+    @State private var progress = 0.5
+
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "applescript")
+                    .font(.title)
+                    .foregroundStyle(.white)
+                    .padding(8)
+                    .background(.red)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                Text("Lorem Ipsum dolor sit")
+                    .font(.title)
+                    .bold()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            ProgressView(value: progress)
+                .tint(.red)
+            HStack {
+                Text("Bab 11 dari 100")
+                    .font(.caption)
+                Spacer()
+                Text("11%")
+                    .font(.caption)
+            }
+            .padding(.top)
+        }
+        .padding()
+        .overlay {
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(.red, lineWidth: 2)
+        }
+        .frame(maxWidth: 300)
+    }
+}
+
+#Preview {
+    ReadingCard()
+}
