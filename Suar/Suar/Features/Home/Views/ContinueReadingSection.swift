fd8bc0e154e2f2906a13fdb1ecd2cbb989116cd1
= <=>
2026-08-26 15:40:10 +0700

    feat: menabahkan desain card untuk homepage

---
+++ Suar/Suar/Features/Home/Views/ContinueReadingSection.swift
@@ -0,0 +1,22 @@
+//
+//  ContinueReadingSection.swift
+//  Suar
+//
+//  Created by Ivan Putra Pratama on 26/08/26.
+//
+
+import SwiftUI
+
+struct ContinueReadingSection: View {
+    var body: some View {
+        VStack {
+            Text("Continue Reading")
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
+
+#Preview {
+    ContinueReadingSection()
+}
