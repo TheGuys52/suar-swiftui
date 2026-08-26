62e8266d132afbb8a63f5fdf1ab1615dd1787471
= <=>
2026-08-26 14:55:56 +0700

    feat: menambahkan routing ke homeview

--- Suar/Suar/Features/Home/HomeViewModel.swift
+++ Suar/Suar/Features/Home/HomeViewModel.swift
@@ -1,12 +1,12 @@
+////
+////  HomeViewModel.swift
+////  Suar
+////
+////  Created by DIMAS DAFFA ERNANDA on 25/08/26.
+////
 //
-//  HomeViewModel.swift
-//  Suar
+// import Foundation
 //
-//  Created by DIMAS DAFFA ERNANDA on 25/08/26.
-//
-
-import Foundation
-
 // @Observable
 // @MainActor
 // public final class HomeViewModel {
