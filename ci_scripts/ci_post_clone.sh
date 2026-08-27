50a1d8d191d3a6bd71cd4675c25a7aba226fb452
dimasdaffa <dimas.de2004@gmail.com>
2026-08-27 15:37:06 +0700

    feat: [ci] set working directory to repository root before running xcodegen

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# 1. Install XcodeGen via Homebrew
 brew install xcodegen
 
+# 2. Pindah direktori ke root utama repositori
+cd "$CI_PRIMARY_REPOSITORY_DIR" || exit 1
+
+# 3. Generate Xcode Project
 xcodegen generate
