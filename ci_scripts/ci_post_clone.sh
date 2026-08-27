077cb3c8bb601316df7eed8c33db203aa6440079
dimasdaffa <dimas.de2004@gmail.com>
2026-08-27 15:43:21 +0700

    feat: [ci] resolve SPM dependencies in post clone script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,8 +3,11 @@
 # 1. Install XcodeGen via Homebrew
 brew install xcodegen
 
-# 2. Pindah direktori ke root utama repositori (naik 1 level dari ci_scripts)
+# 2. Pindah ke root utama repositori
 cd "$(dirname "$0")/.." || exit 1
 
 # 3. Generate Xcode Project
 xcodegen generate
+
+# 4. Resolve Swift Package Manager dependencies untuk membuat Package.resolved
+xcodebuild -resolvePackageDependencies -project Suar.xcodeproj -scheme Suar
