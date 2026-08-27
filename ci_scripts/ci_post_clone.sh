6295382226caf715908e69ab4cd04524fde8909a
dimasdaffa <dimas.de2004@gmail.com>
2026-08-27 15:40:21 +0700

    feat: [ci] fix directory path to parent root for xcodegen

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,8 +3,8 @@
 # 1. Install XcodeGen via Homebrew
 brew install xcodegen
 
-# 2. Pindah direktori ke root utama repositori
-cd "$CI_PRIMARY_REPOSITORY_DIR" || exit 1
+# 2. Pindah direktori ke root utama repositori (naik 1 level dari ci_scripts)
+cd "$(dirname "$0")/.." || exit 1
 
 # 3. Generate Xcode Project
 xcodegen generate
